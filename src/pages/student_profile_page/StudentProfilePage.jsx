4e621465b0de875b220232a3d3c936206db68225
[sardor_user] <sardortohirov222@gmail.com>
2024-02-02 14:33:49 -0700

    student profile page bugs fixed

--- src/pages/student_profile_page/StudentProfilePage.jsx
+++ src/pages/student_profile_page/StudentProfilePage.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import settingService from "../../services/settingService";
 import {jwtDecode} from "jwt-decode";
 import Cookie from "js-cookie";
@@ -11,50 +11,40 @@ import {setStudentProfile} from "../../actions";
 import "./StudentProfilePage.scss"
 
 
-
 const StudentProfilePage = () => {
     const {profile} = useSelector(state => state.profile);
     const dispatch = useDispatch();
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [userName, setUserName] = useState('');
-    const [bio, setBio] = useState('');
-    const [photoUrl, setPhotoUrl] = useState('');
-
     useEffect(() => {
         const token = Cookie.get("access_token");
         const username = jwtDecode(token).sub;
-        console.log("here--------------")
+        console.log('---------------')
+        console.log(username)
         settingService.getStudentProfile(username).then(data => {
+            console.log(data)
             dispatch(setStudentProfile(data))
         });
-        console.log(profile)
-    }, []);
+    }, [dispatch]);
 
-    useEffect(() => {
-        setFirstName(profile?.firstName);
-        setLastName(profile?.lastName);
-        setUserName(profile?.userName);
-        setBio(profile?.bio);
-        setPhotoUrl(profile?.profilePictureUrl);
-    }, [profile]);
+
     return (
         <div className="StudentProfilePage">
             <div className="student-wrapper">
-                <img src={photoUrl==null||photoUrl===''?user_image:photoUrl} alt="user-image" className="user-image"/>
+                <img
+                    src={profile?.profilePictureUrl == null || profile?.profilePictureUrl === '' ? user_image : profile?.profilePictureUrl}
+                    alt="user-image" className="user-image"/>
                 <div className="student-name-wrapper">
-                    <div className="student-username">{userName}</div>
-                    <div className="student-name">{firstName + " " + lastName}</div>
+                    <div className="student-username">{profile?.userName}</div>
+                    <div className="student-name">{profile?.firstName} {profile?.lastName}</div>
                 </div>
                 <Link to="/account/settings">
                     <img src={settings_icon} alt="settings-icon" className="settings-icon"/>
                 </Link>
             </div>
             <div className="student-bio">
-                {bio}
+                {profile?.bio}
             </div>
         </div>
-    )
+    );
 }
 export default StudentProfilePage;
--- src/services/settingService.js
+++ src/services/settingService.js
@@ -1,32 +1,45 @@
 import axios from 'axios';
-import Cookie from "js-cookie";
+import Cookie from 'js-cookie';
 
 const API_BASE_URL = 'http://localhost:8081/api/v1';
 
-const token = Cookie.get("access_token");
-const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-    },
-});
+const axiosErrorHandler = (error) => error?.response?.data;
+
 const settingService = {
     getStudentProfile: async (username) => {
+        const token = Cookie.get('access_token');
+        const axiosInstance = axios.create({
+            baseURL: API_BASE_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
         try {
-            const response = (await axiosInstance.get(`/student/${username}`)).data;
-            return response;
+            const response = await axiosInstance.get(`/student/${username}`);
+            return response.data;
         } catch (error) {
-                return error?.response?.data;
+            return axiosErrorHandler(error);
         }
     },
     getTeacherProfile: async (username) => {
+        const token = Cookie.get('access_token');
+        const axiosInstance = axios.create({
+            baseURL: API_BASE_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
         try {
             const response = await axiosInstance.get(`/teacher/${username}`);
             return response.data;
         } catch (error) {
-            return error?.response?.data;
+            return axiosErrorHandler(error);
         }
     },
-}
+};
+
 export default settingService;
