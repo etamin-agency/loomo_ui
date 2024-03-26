241ce7e43321c36f26da4043b54d341cbf526c39
[sardor_user] <sardortohirov222@gmail.com>
2024-03-26 05:00:06 -0700

    ready

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1/auth';
+const API_BASE_URL = 'http://register.loomo.com:8080/api/v1/auth';
 
 const authService = {
     register: async (request) => {
