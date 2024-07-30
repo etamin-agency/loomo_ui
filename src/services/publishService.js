dabbae467eb6595c254ad610d2552a8247d191ed
[sardor_user] <sardortohirov222@gmail.com>
2024-07-29 22:52:18 -0700

    Creating Post List page and some hot fixes

--- src/services/publishService.js
+++ src/services/publishService.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import Cookie from 'js-cookie';
 import {jwtDecode} from "jwt-decode";
 
-const API_BASE_URL = 'http://192.168.23.248:8082/api/v1';
+const API_BASE_URL = 'http://192.168.23.248:8082/api/v1/publish-class';
+
+// const API_BASE_URL = 'http://localhost:8082/api/v1/publish-class';
 
 
 const publishService = {
-    getPublishedClasses: async (offset = 0, num = 6, username) => {
+    getPosts: async (offset = 0, num = 6, username) => {
         const token = Cookie.get('access_token');
         const axiosInstance = axios.create({
             baseURL: API_BASE_URL,
@@ -17,7 +19,7 @@ const publishService = {
         });
 
         try {
-            const response = await axiosInstance.get(`/publish-class/${username}?number=${num}&offset=${offset}`);
+            const response = await axiosInstance.get(`/${username}?number=${num}&offset=${offset}`);
             return response.data;
         } catch (error) {
             console.log(error)
@@ -34,7 +36,7 @@ const publishService = {
         });
 
         try {
-            const response = await axiosInstance.get(`/publish-class/languages`);
+            const response = await axiosInstance.get(`/languages`);
             return response.data;
         } catch (error) {
             console.log(error)
@@ -52,7 +54,7 @@ const publishService = {
         const userName = jwtDecode(Cookie.get('access_token')).sub;
 
         try {
-            const response =    await axiosInstance.post(`/publish-class/${userName}/create`,formData);
+            const response =    await axiosInstance.post(`/${userName}/create`,formData);
             return response.data;
         } catch (error) {
             console.log(error)
@@ -69,7 +71,7 @@ const publishService = {
         });
 
         try {
-            const response = await axiosInstance.get(`/publish-class/post/${uuid}`);
+            const response = await axiosInstance.get(`/post/${uuid}`);
             return response.data;
         } catch (error) {
             console.log(error)
@@ -86,7 +88,7 @@ const publishService = {
             },
         });
         try {
-            const response = await axiosInstance.get(`/publish-class/post/image/${uuid}`);
+            const response = await axiosInstance.get(`/post/image/${uuid}`);
             return response.data;
         } catch (error) {
             console.log(error)
@@ -103,7 +105,7 @@ const publishService = {
         });
 
         try {
-            const response =  await axiosInstance.post(`/publish-class/edit/${uuid}`,formData);
+            const response =  await axiosInstance.post(`edit/${uuid}`,formData);
             console.log(response)
             return response.data;
         } catch (error) {
