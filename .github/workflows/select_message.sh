de479491a2e89b44d52299f9c053e5b6a52f119c
[sardor_user] <sardortohirov222@gmail.com>
2024-08-07 22:03:29 -0700

    bot testing

--- .github/workflows/select_message.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MESSAGES=(
-  "A new commit just dropped! 🚀\n\n👨‍💻 **Author:** ${GITHUB_COMMIT_AUTHOR}\n📜 **Message:** ${GITHUB_COMMIT_MESSAGE}\n\nGreat job, team! 🌟"
-)
-RANDOM_INDEX=$((RANDOM % ${#MESSAGES[@]}))
-
-echo "${MESSAGES[$RANDOM_INDEX]}"
