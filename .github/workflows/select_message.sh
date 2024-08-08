8c599d8963202cc557dfee60e42e523343078d68
[sardor_user] <sardortohirov222@gmail.com>
2024-08-07 21:58:46 -0700

    beautiful packaging for commits

--- .github/workflows/select_message.sh
+++ .github/workflows/select_message.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Array of informal motivational messages
 MESSAGES=(
   "A new commit just dropped! 🚀\n\n👨‍💻 **Author:** ${GITHUB_COMMIT_AUTHOR}\n📜 **Message:** ${GITHUB_COMMIT_MESSAGE}\n\nGreat job, team! 🌟"
 )
